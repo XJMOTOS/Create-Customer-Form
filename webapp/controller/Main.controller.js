915640c41c629ec42afa8f4ae748246c8b42aaf0
Jorge Motos (XJMOTOS) <jorge.motos.digittude@BeamSuntory.com>
2025-07-30 11:34:55 +0200

    Transform to Wizard and modify fields

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -18,74 +18,134 @@ sap.ui.define([
 		_initializeModels: function () {
 			this._loadModelFromJSONFile("view", "model/view.json");
 			this._loadModelFromJSONFile("customer", "model/customer.json");
-			this._loadModelFromJSONFile("taxInformation", "model/data/taxInformation.json");
-			this._loadModelFromJSONFile("taxClasification", "model/data/taxClassification.json");
-			this._loadModelFromJSONFile("contactType", "model/data/contactType.json");
-			this._loadModelFromJSONFile("distributtionChannel", "model/data/distributtionChannel.json");
-			this._loadModelFromJSONFile("division", "model/data/division.json");
-			this._loadModelFromJSONFile("paymentTerms", "model/data/paymentTerms.json");
-			this._loadModelFromJSONFile("accountAssignmentGroup", "model/data/accountAssignmentGroup.json");
-			this.getView().setModel(new JSONModel([]), "customerContact");
+
+			// this._loadModelFromJSONFile("taxInformation", "model/data/taxInformation.json");
+			// this._loadModelFromJSONFile("taxClasification", "model/data/taxClassification.json");
+			// this._loadModelFromJSONFile("contactType", "model/data/contactType.json");
+			// this._loadModelFromJSONFile("distributtionChannel", "model/data/distributtionChannel.json");
+			// this._loadModelFromJSONFile("division", "model/data/division.json");
+			// this._loadModelFromJSONFile("paymentTerms", "model/data/paymentTerms.json");
+			// this._loadModelFromJSONFile("accountAssignmentGroup", "model/data/accountAssignmentGroup.json");
 		},
 
-		_toggleVisibility: function (modelName, property) {
-			let model = this.getView().getModel(modelName);
+		_toggleVisibility: function (property) {
+			let model = this.getView().getModel("view");
 			let visible = model.getProperty(property);
 			model.setProperty(property, !visible);
 		},
 
-		onToggleTaxTable: function (event) {
-			this._toggleVisibility("view", "/show/table/taxInformation");
-		},
-
-		onToggleCustomerContactTable: function (event) {
-			this._toggleVisibility("view", "/show/table/customerContact");
-		},
-
-		onToggleOrganizationalDataOptionalFields: function (event) {
-			this._toggleVisibility("view", "/show/optionalOrganizationalDataFields");
-		},
-
-		onToggleGeneralDataOptionalFields: function (event) {
-			this._toggleVisibility("view", "/show/optionalGeneralDataFields");
-		},
-
-		onShowTaxNumber: function (event) {
+		_showsField: function (fieldName, limit) {
 			let model = this.getView().getModel("view");
-			let visible = model.getProperty("/show/taxNumber2");
-			if (!visible) {
-				model.setProperty("/show/taxNumber2", true);
-				return
+			for (let index = 2; index <= limit; index++) {
+				let visible = model.getProperty(`/show/${fieldName}${index}`);
+				if (!visible) {
+					model.setProperty(`/show/${fieldName}${index}`, true);
+					return;
+				}
 			}
-			model.setProperty("/show/taxNumber4", true);
 		},
 
-		onHideTaxNumber: function (event, param) {
+		onToggleTaxDetailsFields: function (event) {
+			this._toggleVisibility("/show/taxDetailsFields");
+		},
+
+		onToggleAttributeFields: function (event) {
+			this._toggleVisibility("/show/attributeFields");
+		},
+
+		onToggleAddressFields: function (event) {
+			this._toggleVisibility("/show/addressFields");
+		},
+
+		onToggleContactPersonsTable: function (event) {
+			this._toggleVisibility("/show/contactPersonsTable");
+		},
+		
+		onToggleCompanyFields: function (event) {
+			this._toggleVisibility("/show/companyFields");
+		},
+
+		onToggleDeliveryFields: function (event) {
+			this._toggleVisibility("/show/deliveryFields");
+		},
+		
+		onToggleCustomerGroupFields: function (event) {
+			this._toggleVisibility("/show/customerGroupFields");
+		},
+
+		onToggleSalesFields: function (event) {
+			this._toggleVisibility("/show/salesFields");
+		},
+		
+		onToggleHierarchyFields: function (event) {
+			this._toggleVisibility("/show/hierarchyFields");
+		},
+
+		onShowName: function (event) {
+			this._showsField("name", 4);
+		},
+
+		onHideName: function (event, param) {
 			let model = this.getView().getModel("view");
-			model.setProperty(`/show/taxNumber${param}`, false);
+			model.setProperty(`/show/name${param}`, false);
 		},
 
-		onAddContact: function (event) {
-			let model = event.getSource().getModel("customerContact");
-			let contacts = model.getData();
+		onShowTelephone: function (event) {
+			this._showsField("telephone", 5);
+		},
+
+		onHideTelephone: function (event, param) {
+			let model = this.getView().getModel("view");
+			model.setProperty(`/show/telephone${param}`, false);
+		},
+
+		onShowFax: function (event) {
+			this._showsField("fax", 5);
+		},
+
+		onHideFax: function (event, param) {
+			let model = this.getView().getModel("view");
+			model.setProperty(`/show/fax${param}`, false);
+		},
+
+		onShowEmail: function (event) {
+			this._showsField("email", 5);
+		},
+
+		onHideEmail: function (event, param) {
+			let model = this.getView().getModel("view");
+			model.setProperty(`/show/email${param}`, false);
+		},
+
+		onShowStreet: function (event) {
+			this._showsField("street", 5);
+		},
+
+		onHideStreet: function (event, param) {
+			let model = this.getView().getModel("view");
+			model.setProperty(`/show/street${param}`, false);
+		},
+
+		onAddContactPerson: function (event) {
+			let model = event.getSource().getModel("customer");
+			let contacts = model.getProperty("/contactPersons");
 			contacts.push({
-				contactType: "",
-				name: "",
-				surname: "",
-				phone: "",
-				fax: "",
-				email: ""
+				function: "",
+				firstName: "",
+				lastName: "",
+				email: "",
+				phone: ""
 			});
-			model.setData(new JSONModel(contacts), "customerContact");
+			model.setProperty("/contactPersons", contacts);
 		},
 
-		onRemoveContact: function (event) {
-			let model = event.getSource().getModel("customerContact");
-			let contacts = model.getData();
-			let path = event.getParameter("listItem").getBindingContext("customerContact").getPath();
-			let index = parseInt(path.slice(1), 10);
+		onRemoveContactPerson: function (event) {
+			let model = event.getSource().getModel("customer");
+			let contacts = model.getProperty("/contactPersons");
+			let path = event.getParameter("listItem").getBindingContext("customer").getPath();
+			let index = path.split("/")[2];
 			contacts.splice(index, 1);
-			model.setData(new JSONModel(contacts), customerContactModelName);
+			model.setProperty("/contactPersons", contacts);
 		}
 	});
 });
